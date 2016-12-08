ec1f22b2e9d48333c5cf8e6df27dc24a4c74b6a0
Gregor Flajs <gregor.flajs@ors.si>
2016-12-08 16:49:20 +0100

    moved SmartAutocompleteInterface in a sepparate file

--- src/Ors/Support/SmartAutocomplete.php
+++ src/Ors/Support/SmartAutocomplete.php
@@ -4,42 +4,6 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Contracts\ArrayableInterface;
 
 /**
- * Models that you want to use for autocomplete functionallity must implement this interface.
- * 
- * You can also use a callback when creating SmartAutocomplete object, if you don't wish to use this interface.
- * 
- * @author Gregor Flajs
- *
- */
-interface SmartAutocompleteInterface {
-	
-	/**
-	 * a key value
-	 * @return string
-	 */
-	public function getSmartAutocompleteKeyAttribute();
-	
-	/**
-	 * display item in the list
-	 * @return string
-	 */
-	public function getSmartAutocompleteTitleAttribute();
-	
-	/**
-	 * title for a tab (if this is empty then SmartAutocompleteTitle is used)
-	 * @return string
-	 */
-	public function getSmartAutocompleteTabTitleAttribute();
-	
-	/**
-	 * css class for icon to display in front of each item (this can be empty)
-	 * @return string
-	 */
-	public function getSmartAutocompleteIconAttribute();
-	
-}
-
-/**
  * SmartAutocomplete class prepares a list of items for jquery-autocomplete function.
  * 
  * In order for SmartAutocomplete to work, objects in $collection must implement SmartAutocompleteInterface,
